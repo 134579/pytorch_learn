1ff8d00517bfe2b6187be5738bd8bc56d6eeef44
laptop <1203949553@qq.com>
2018-10-04 21:55:20 +0800

    implement lin reg use native torch

--- pytorch_in_10_min/5_rnn.py
+++ pytorch_in_10_min/5_rnn.py
@@ -1,11 +1,14 @@
 import torch
 import torch.nn as nn
+from torch.nn import Parameter
 
 
 class RNN(torch.nn.Module):
     def __init__(self, in_dimen, out_dimen):
+        super().__init__()
+        wih = Parameter(torch.Tensor())
         nn.RNNCell
-        nn.RNNBase
         nn.RNN
-        
-        
+
+    def forward(self, x: torch.Tensor):
+        pass
