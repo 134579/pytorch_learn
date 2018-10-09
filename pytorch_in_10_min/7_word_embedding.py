d015a283a6d8ec5773cea1d23785975ee7d11de3
huangkai <hk134579@sina.com>
2018-10-09 09:29:32 +0800

    finish 8 word embedding

--- pytorch_in_10_min/7_word_embedding.py
+++ pytorch_in_10_min/7_word_embedding.py
@@ -6,6 +6,6 @@ if __name__ == '__main__':
     word_to_ix = {'hello': 0, 'world': 1}
     embeds = nn.Embedding(2, 5)
     hello_idx = torch.LongTensor([word_to_ix['hello']])
-    hello_idx = Variable(hello_idx)
+    # hello_idx = Variable(hello_idx)
     hello_embed = embeds(hello_idx)
     print(hello_embed)
--- pytorch_in_10_min/8_word_prediction.py
+++ pytorch_in_10_min/8_word_prediction.py
@@ -1,12 +1,20 @@
-from torch import nn
+import torch
+
+from torch import nn, optim
 import torch.nn.functional as F
 
 
 class NgramModel(nn.Module):
     def __init__(self, vocb_size, context_size, n_dim):
+        """
+        
+        :param vocb_size:
+        :param context_size: n of `ngram`
+        :param n_dim:
+        """
         super(NgramModel, self).__init__()
         self.n_word = vocb_size
-        self.embedding = nn.Embedding(self.n_word, n_dim)
+        self.embedding = nn.Embedding(self.n_word, n_dim)  # number of all word, dimension of one word
         self.linear1 = nn.Linear(context_size * n_dim, 128)
         self.linear2 = nn.Linear(128, self.n_word)
     
@@ -16,8 +24,8 @@ class NgramModel(nn.Module):
         out = self.linear1(emb)
         out = F.relu(out)
         out = self.linear2(out)
-        log_prob = F.log_softmax(out)
-        return log_prob
+        prob = F.softmax(out)
+        return prob
 
 
 if __name__ == '__main__':
@@ -45,3 +53,33 @@ if __name__ == '__main__':
     vocb = set(test_sentence)  # 通过set将重复的单词去掉
     word_to_idx = {word: i for i, word in enumerate(vocb)}
     idx_to_word = {word_to_idx[word]: word for word in word_to_idx}
+    
+    ngrammodel = NgramModel(len(word_to_idx), CONTEXT_SIZE, 100)
+    criterion = nn.NLLLoss()
+    optimizer = optim.Adam(ngrammodel.parameters(), lr=1e-3)
+    
+    epochs = 40
+    
+    for epoch in range(epochs):
+        running_loss = 0
+        for data in trigram:
+            word, label = data
+            word = torch.LongTensor([word_to_idx[i] for i in word])
+            label = torch.LongTensor([word_to_idx[label]])
+            # forward
+            out = ngrammodel(word)
+            loss = criterion(out, label)
+            running_loss = loss.data.item()
+            # backward
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+        print(f'Loss: {running_loss / len(word_to_idx):.6f}')
+    
+    with torch.no_grad():
+        word, label = trigram[3]
+        word_ids = (torch.LongTensor([word_to_idx[i] for i in word]))
+        out = ngrammodel(word_ids)
+        _, predict_label = torch.max(out, 1)
+        predict_word = idx_to_word[predict_label.item()]
+        print(f'{word}, real word is {label}, predict word is {predict_word}')
